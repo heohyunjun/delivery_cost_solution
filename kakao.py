b207976b5fb0541ec94ac74c8cfc232d1eb6077c
heohyunjun <hhj2676@naver.com>
2022-05-05 14:03:49 +0900

    feat : get_text 카카오톡 채팅방의 대화를 가져오는 기능을 추가함 - arifox1님 블로그 참조

--- kakao.py
+++ kakao.py
@@ -1,7 +1,27 @@
-import time, win32con, win32api, win32gui
+import time, win32con, win32api, win32gui, ctypes
+from pywinauto import clipboard
 kakao_room_name = '대학'
+PBYTE256 = ctypes.c_ubyte * 256
+_user32 = ctypes.WinDLL("user32")
+GetKeyboardState = _user32.GetKeyboardState
+SetKeyboardState = _user32.SetKeyboardState
+PostMessage = win32api.PostMessage
+SendMessage = win32gui.SendMessage
+FindWindow = win32gui.FindWindow
+IsWindow = win32gui.IsWindow
+GetCurrentThreadId = win32api.GetCurrentThreadId
+GetWindowThreadProcessId = _user32.GetWindowThreadProcessId
+AttachThreadInput = _user32.AttachThreadInput
 
+MapVirtualKeyA = _user32.MapVirtualKeyA
+MapVirtualKeyW = _user32.MapVirtualKeyW
 
+MakeLong = win32api.MAKELONG
+# # 핸들
+hwndMain = win32gui.FindWindow( None, kakao_room_name)
+hwndEdit = win32gui.FindWindowEx( hwndMain, None, "RICHEDIT50W", None)
+hwndListControl = win32gui.FindWindowEx( hwndMain, None, "EVA_VH_ListControl_Dblclk", None)
+w = win32con
 def kakao_sendtext(text):
     win32api.SendMessage(hwndEdit, win32con.WM_SETTEXT, 0, text)
     SendReturn(hwndEdit)
@@ -12,11 +32,83 @@ def SendReturn(hwnd):
     time.sleep(1)
     win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
 
-# # 핸들
-hwndMain = win32gui.FindWindow( None, kakao_room_name)
-hwndEdit = win32gui.FindWindowEx( hwndMain, None, "RICHEDIT50W", None)
-hwndListControl = win32gui.FindWindowEx( hwndMain, None, "EVA_VH_ListControl_Dblclk", None)
+# # 채팅내용 가져오기
+def copy_chatroom(chatroom_name):
+    # # 핸들 _ 채팅방
+    hwndMain = win32gui.FindWindow( None, chatroom_name)
+    hwndListControl = win32gui.FindWindowEx(hwndMain, None, "EVA_VH_ListControl_Dblclk", None)
+
+    # #조합키, 본문을 클립보드에 복사 ( ctl + c , v )
+    PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
+    time.sleep(1)
+    PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
+    ctext = clipboard.GetData()
+    # print(ctext)
+    return ctext
+
+
+# 조합키 쓰기 위해
+def PostKeyEx(hwnd, key, shift, specialkey):
+    if IsWindow(hwnd):
+
+        ThreadId = GetWindowThreadProcessId(hwnd, None)
+
+        lparam = MakeLong(0, MapVirtualKeyA(key, 0))
+        msg_down = w.WM_KEYDOWN
+        msg_up = w.WM_KEYUP
+
+        if specialkey:
+            lparam = lparam | 0x1000000
+
+        if len(shift) > 0:
+            pKeyBuffers = PBYTE256()
+            pKeyBuffers_old = PBYTE256()
+
+            SendMessage(hwnd, w.WM_ACTIVATE, w.WA_ACTIVE, 0)
+            AttachThreadInput(GetCurrentThreadId(), ThreadId, True)
+            GetKeyboardState(ctypes.byref(pKeyBuffers_old))
+
+            for modkey in shift:
+                if modkey == w.VK_MENU:
+                    lparam = lparam | 0x20000000
+                    msg_down = w.WM_SYSKEYDOWN
+                    msg_up = w.WM_SYSKEYUP
+                pKeyBuffers[modkey] |= 128
+
+            SetKeyboardState(ctypes.byref(pKeyBuffers))
+            time.sleep(0.01)
+            PostMessage(hwnd, msg_down, key, lparam)
+            time.sleep(0.01)
+            PostMessage(hwnd, msg_up, key, lparam | 0xC0000000)
+            time.sleep(0.01)
+            SetKeyboardState(ctypes.byref(pKeyBuffers_old))
+            time.sleep(0.01)
+            AttachThreadInput(GetCurrentThreadId(), ThreadId, False)
+
+        else:
+            SendMessage(hwnd, msg_down, key, lparam)
+            SendMessage(hwnd, msg_up, key, lparam | 0xC0000000)
+
 
 # # 채팅 전송
-text = "하이"
-kakao_sendtext(text)
+# text = "하이"
+# kakao_sendtext(text)
+def main():
+
+    # # 핸들 _ 채팅방
+    hwndMain = win32gui.FindWindow( None, kakao_room_name)
+    hwndListControl = win32gui.FindWindowEx(hwndMain, None, "EVA_VH_ListControl_Dblclk", None)
+
+    # #조합키, 본문을 클립보드에 복사 ( ctl + A , C )
+    PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
+    time.sleep(1)
+    PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
+    ctext = clipboard.GetData()
+
+    # 마지막 텍스트만 가져옴
+    ctext = list(ctext.split('\r'))[-2]
+    print(ctext)# 내용 확인
+
+
+if __name__ == '__main__':
+    main()
